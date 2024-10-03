e8c13833aee0c5093f6da7da413da7da02166289
Trung Nguyen <thanhtrungnguyen1208@gmail.com>
2024-10-03 15:27:22 +0700

    Project Setup and Shared Components

---
+++ SwiftulSwiftUIPractive/Shared/ImageLoaderView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageLoaderView.swift
+//  SwiftulSwiftUIPractive
+//
+//  Created by sotatek on 3/10/24.
+//
+
+import SwiftUI
+
+struct ImageLoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+}
