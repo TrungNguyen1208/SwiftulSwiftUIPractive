aedb282622dd67bbb10c1f47a98e23fe0f713ba9
Trung Nguyen <trung.nguyen@sotatek.com>
2024-10-03 14:39:04 +0700

    Initial Commit

---
+++ SwiftulSwiftUIPractive/SwiftulSwiftUIPractiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulSwiftUIPractiveApp.swift
+//  SwiftulSwiftUIPractive
+//
+//  Created by sotatek on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulSwiftUIPractiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
